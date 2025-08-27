54855f4e472b5fc5ea3906374305419fb687273e
HanzhangYin <y2315657163@gmail.com>
2025-08-27 13:13:01 -0500

    Update .gitignore to include .env file and remove run_evolution from __all__ in __init__.py

--- openevolve/__init__.py
+++ openevolve/__init__.py
@@ -4,6 +4,5 @@ OpenEvolve: An open-source implementation of AlphaEvolve
 
 from openevolve._version import __version__
 from openevolve.controller import OpenEvolve
-from openevolve.evolution import run_evolution
 
-__all__ = ["OpenEvolve", "__version__", "run_evolution"]
+__all__ = ["OpenEvolve", "__version__"]
